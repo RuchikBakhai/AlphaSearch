f76158d23aa1322cdf7f5b69f75b5bdee49a6032
Ruchik Bakhai <f20212054@goa.bits-pilani.ac.in>
2022-06-11 15:24:39 +0530

    initial commit
    added all files

---
+++ index.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+  $(".heading").css("display","none");
+  $(".heading").fadeIn(1000);
+  $(".search").css("display","none");
+  $(".search").fadeIn(1000);
+  $(".engine").css("display","none");
+  $(".engine").fadeIn(1000);
+  $(".searchimg").css("display","none");
+  $(".searchimg").fadeIn(1000);
+  $(".rules").css("display","none");
+  $(".rules").fadeIn(1000);
+});
+var es = false;
+var engine = "";
+$("body").on("keypress",function(event){
+  if(event.key=="Enter"){
+    if(es==false){
+      var key = document.querySelector(".searchbar").value;
+      es = true;
+      setEngine(key);
+    }
+    else{
+      var key = document.querySelector(".searchbar").value;
+      search(key);
+    }
+  }
+});
+$(".searchimg").click(function(event){
+      if(es==false){
+        var key = document.querySelector(".searchbar").value;
+        es = true;
+        setEngine(key);
+      }
+      else{
+        var key = document.querySelector(".searchbar").value;
+        search(key);
+      }
+});
+function setEngine(key){
+  if(key=="g"){
+    $(".enginename").text("Google");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Google");
+    engine = "https://www.google.com/search?q=";
+  }
+  else if(key=="y"){
+    $(".enginename").text("Youtube");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Youtbe");
+    engine = "https://www.youtube.com/results?search_query=";
+  }
+  else if(key=="d"){
+    $(".enginename").text("Google Docs");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Google Docs");
+    engine = "https://docs.google.com/document/u/0/?tgif=d&q=";
+  }
+  else if(key=="f"){
+    $(".enginename").text("Facebook");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Facebook");
+    engine = "https://www.facebook.com/search/top?q=";
+  }
+  else if(key=="sl"){
+    $(".enginename").text("Slides");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Slides");
+    engine = "https://docs.google.com/presentation/u/0/?tgif=d&q=";
+  }
+  else if(key=="dr"){
+    $(".enginename").text("Drive");
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("search in Drive");
+    engine = "https://drive.google.com/drive/search?q=";
+  }
+  else{
+    var key = document.querySelector(".searchbar").value = "";
+    $(".codehead").text("Invalid key");
+    es = false;
+  }
+}
+
+function search(key){
+  window.location.href = engine+key;
+}
